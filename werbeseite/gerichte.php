4433dbad7de042ed879e93467b3873d451510350
yanniksn <ysinthern@gmail.com>
2023-11-08 18:29:35 +0100

    mein zeugs

--- werbeseite/gerichte.php
+++ werbeseite/gerichte.php
@@ -4,6 +4,7 @@
  * Yannik, Sinthern, 3570151
  * Haider, Abbas, 3567272
  */
+
 $gerichte = array(
     1 => ['name' => 'Rindfleisch mit Bambus, Kaiserschoten und rotem Paprika, dazu Mie Nudeln',
         'preisIntern' => '3,50',
